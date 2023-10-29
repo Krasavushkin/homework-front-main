19e8f28dcc89cb0d76a6f98ab771c968352ecea7
krasavushkin <artemkrasavushkin@gmail.com>
2023-10-29 18:00:43 +0300

    Done HM2

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,11 +1,12 @@
-import { UserType } from './HW2';
+import {UserType} from './HW2';
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: UserType  // пропиши типизацию
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
-  return (
+
+    return (
     <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
       <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
       {props.user.address.street}, {props.user.address.city}
